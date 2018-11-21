b604656ec6149bb255d46ec2385e0442bde99048
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2018-11-21 16:04:32 -0700

    final

--- main.js
+++ main.js
@@ -1,138 +1,145 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var box1 = document.getElementById("1");
-    var box2 = document.getElementById("2");
-    var box3 = document.getElementById("3");
-    var box4 = document.getElementById("4");
-    var box5 = document.getElementById("5");
-    var box6 = document.getElementById("6");
-    var box7 = document.getElementById("7");
-    var box8 = document.getElementById("8");
-    var box9 = document.getElementById("9");
-    var box10 = document.getElementById("10");
-    var box11 = document.getElementById("11");
-    var box12 = document.getElementById("12");
-    var box13 = document.getElementById("13");
-    var box14 = document.getElementById("14");
-    var box15 = document.getElementById("15");
-    var box16 = document.getElementById("16");
-    var canvas1 = document.getElementById("canvas1");
-    var canvas2 = document.getElementById("canvas2");
-    var canvas3 = document.getElementById("canvas3");
-    var canvas4 = document.getElementById("canvas4");
-    var canvas5 = document.getElementById("canvas5");
-    var canvas6 = document.getElementById("canvas6");
-    var canvas7 = document.getElementById("canvas7");
-    var canvas8 = document.getElementById("canvas8");
-    var canvas9 = document.getElementById("canvas9");
-    var indicator = document.getElementById("indicator");
-    let color 
+    var box1 = document.getElementById("1")
+    var box2 = document.getElementById("2")
+    var box3 = document.getElementById("3")
+    var box4 = document.getElementById("4")
+    var box5 = document.getElementById("5")
+    var box6 = document.getElementById("6")
+    var box7 = document.getElementById("7")
+    var box8 = document.getElementById("8")
+    var box9 = document.getElementById("9")
+    var box10 = document.getElementById("10")
+    var box11 = document.getElementById("11")
+    var box12 = document.getElementById("12")
+    var box13 = document.getElementById("13")
+    var box14 = document.getElementById("14")
+    var box15 = document.getElementById("15")
+    var box16 = document.getElementById("16")
+    var canvas1 = document.getElementById("canvas1")
+    var canvas2 = document.getElementById("canvas2")
+    var canvas3 = document.getElementById("canvas3")
+    var canvas4 = document.getElementById("canvas4")
+    var canvas5 = document.getElementById("canvas5")
+    var canvas6 = document.getElementById("canvas6")
+    var canvas7 = document.getElementById("canvas7")
+    var canvas8 = document.getElementById("canvas8")
+    var canvas9 = document.getElementById("canvas9")
+    var canvas10 = document.getElementById("canvas10")
+    var indicator = document.getElementById("indicator")
+    let color = ""
     canvas1.addEventListener("click", () => {
         color = "red"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas2.addEventListener("click", () => {
         color = "blue"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas3.addEventListener("click", () => {
         color = "green"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas4.addEventListener("click", () => {
         color = "yellow"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas5.addEventListener("click", () => {
         color = "orange"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas6.addEventListener("click", () => {
         color = "purple"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas7.addEventListener("click", () => {
         color = "brown"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas8.addEventListener("click", () => {
         color = "black"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
+        indicator.classList.toggle("inherit")
       })
     canvas9.addEventListener("click", () => {
         color = "gray"
         indicator.classList.toggle(color)
-        indicator.classList.toggle("none")
-      })    
+        indicator.classList.toggle("inherit")
+      })
+    canvas10.addEventListener("click", () => {
+        color = "white"
+        indicator.classList.toggle(color)
+        indicator.classList.toggle("inherit")
+      })
+      
     box1.addEventListener("click", () => {
         box1.classList.toggle(color)
-        box1.classList.toggle("none")
+        box1.classList.toggle("inherit")
       })
     box2.addEventListener("click", () => {
         box2.classList.toggle(color)
-        box2.classList.toggle("none")
+        box2.classList.toggle("inherit")
       })
     box3.addEventListener("click", () => {
         box3.classList.toggle(color)
-        box3.classList.toggle("none")
+        box3.classList.toggle("inherit")
       })
     box4.addEventListener("click", () => {
         box4.classList.toggle(color)
-        box4.classList.toggle("none")
+        box4.classList.toggle("inherit")
       })
     box5.addEventListener("click", () => {
         box5.classList.toggle(color)
-        box5.classList.toggle("none")
+        box5.classList.toggle("inherit")
       })
     box6.addEventListener("click", () => {
         box6.classList.toggle(color)
-        box6.classList.toggle("none")
+        box6.classList.toggle("inherit")
       })
     box7.addEventListener("click", () => {
         box7.classList.toggle(color)
-        box7.classList.toggle("none")
+        box7.classList.toggle("inherit")
       })
     box8.addEventListener("click", () => {
         box8.classList.toggle(color)
-        box8.classList.toggle("none")
+        box8.classList.toggle("inherit")
       })
     box9.addEventListener("click", () => {
         box9.classList.toggle(color)
-        box9.classList.toggle("none")
+        box9.classList.toggle("inherit")
       })
     box10.addEventListener("click", () => {
         box10.classList.toggle(color)
-        box10.classList.toggle("none")
+        box10.classList.toggle("inherit")
       })
     box11.addEventListener("click", () => {
         box11.classList.toggle(color)
-        box11.classList.toggle("none")
+        box11.classList.toggle("inherit")
       })
     box12.addEventListener("click", () => {
         box12.classList.toggle(color)
-        box12.classList.toggle("none")
+        box12.classList.toggle("inherit")
       })
     box13.addEventListener("click", () => {
         box13.classList.toggle(color)
-        box13.classList.toggle("none")
+        box13.classList.toggle("inherit")
       })
     box14.addEventListener("click", () => {
         box14.classList.toggle(color)
-        box14.classList.toggle("none")
+        box14.classList.toggle("inherit")
       })
     box15.addEventListener("click", () => {
         box15.classList.toggle(color)
-        box15.classList.toggle("none")
+        box15.classList.toggle("inherit")
       })
     box16.addEventListener("click", () => {
         box16.classList.toggle(color)
-        box16.classList.toggle("none")
+        box16.classList.toggle("inherit")
       })
 })
